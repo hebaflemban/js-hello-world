ce1ddbab030d72f20c9d66ea50324f66cafe3122
Heba <36724067+heba@users.noreply.github.com>
2020-09-24 13:10:27 +0300

    fix

--- story.js
+++ story.js
@@ -1,12 +1,6 @@
-let name = "NAME"
-let age = "AGE_PLUS_33"
-let hobby = "HOBBY"
 
-
-console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
-
-name = "Heba"
-age = 28
-hobby = "drawing"
+let name = "Heba"
+let age = 28
+let hobby = "drawing"
 
 console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
