b42b1f471f830cd2b37d4e62e2539c74d193f482
Heba <36724067+heba@users.noreply.github.com>
2020-09-24 13:00:18 +0300

    JS's Hello Wosld

---
+++ story.js
@@ -0,0 +1,12 @@
+let name = "NAME"
+let age = "AGE_PLUS_33"
+let hobby = "HOBBY"
+
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
+
+name = "Heba"
+age = 28
+hobby = "drawing"
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
